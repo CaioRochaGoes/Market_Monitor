5f9e3ecc35eaf769314773dc81f31a92feb8cdcd
Caior15 <caiorochagoes100@gmail.com>
2021-12-05 03:25:24 -0300

    add magic Number function

--- Market_Monitor/Fii.cs
+++ Market_Monitor/Fii.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
@@ -56,6 +57,77 @@ namespace Market_Monitor
 				return double.NaN;
 			}
 			
+		}
+		public List<Fii> GetFiis()
+		{
+			try
+			{
+				var p_html = @"https://www.fundamentus.com.br/fii_resultado.php";
+
+				HtmlWeb p_web = new HtmlWeb();
+
+				var p_htmlDoc = p_web.Load(p_html);
+
+				var p_bodyTable = p_htmlDoc.DocumentNode.SelectSingleNode("//body//div//table");
+				p_htmlDoc = new HtmlDocument();
+				p_htmlDoc.LoadHtml(p_bodyTable.OuterHtml);
+				var p_trs = p_htmlDoc.DocumentNode.SelectNodes("//tr");
+				int counter = 0;
+				p_htmlDoc = new HtmlDocument();
+
+				List<Fii> l_fiis = new List<Fii>();
+				Console.WriteLine("Carregando FII´s");
+				foreach (var item in p_trs)
+				{
+
+					if (counter > 0)
+					{
+						Fii p_fii = new Fii();
+						p_htmlDoc.LoadHtml(item.OuterHtml);
+						p_fii.Name = p_htmlDoc.DocumentNode.SelectSingleNode("//td//span//a").InnerText;
+						p_fii.Segment = p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[contains(@ckass,'res_papel')][2]").InnerText;
+						p_fii.Price = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[3]").InnerText);
+						p_fii.FfoYield = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[4]").InnerText);
+						p_fii.DividendYield = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[5]").InnerText);
+						p_fii.P_VP = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[6]").InnerText);
+						p_fii.MarketValue = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[7]").InnerText);
+						p_fii.Liquidity = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[8]").InnerText);
+						p_fii.RealEstateQuantity = int.Parse(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[9]").InnerText);
+						p_fii.PricePerM2 = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[10]").InnerText);
+						p_fii.RentPerM2 = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[11]").InnerText);
+						p_fii.CapRate = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[12]").InnerText);
+						p_fii.AverageVacancy = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[12]").InnerText);
+						l_fiis.Add(p_fii);
+					}
+					counter += 1;
+				}
+				Console.WriteLine(l_fiis.Count + " FII´s Carregados");
+				return l_fiis;
+			}
+			catch (Exception ex)
+			{
+				//Console.WriteLine(ex.Message);
+				throw ex;
+			}
+			
+		}
+
+		public double CalculateMagicNumberByName(string p_name, List<Fii> l_fii)
+		{
+			try
+			{
+				Fii fii = new Fii();
+				double p_quote = 0.90;
+				fii = l_fii.Find(p => fii.Name == p_name);
+				double result = 90.65/ p_quote;//fii.Price / p_quote; 
+				return Math.Round(result);
+			}
+			catch (Exception ex)
+			{
+
+				throw ex;
+			}
+			return double.NaN;
 		}
 		#endregion
 		//enum QualityClassifier
--- Market_Monitor/Program.cs
+++ Market_Monitor/Program.cs
@@ -11,46 +11,32 @@ namespace Market_Monitor
 		
 		static void Main(string[] args)
 		{
-			var p_html = @"https://www.fundamentus.com.br/fii_resultado.php";
-
-			HtmlWeb p_web = new HtmlWeb();
-
-			var p_htmlDoc = p_web.Load(p_html);
-
-			var p_bodyTable = p_htmlDoc.DocumentNode.SelectSingleNode("//body//div//table");
-			p_htmlDoc = new HtmlDocument();
-			p_htmlDoc.LoadHtml(p_bodyTable.OuterHtml);
-			var p_trs = p_htmlDoc.DocumentNode.SelectNodes("//tr");
-			int counter = 0;
-			p_htmlDoc = new HtmlDocument();
-			
-			List<Fii> l_fiis = new List<Fii>();
-			Console.WriteLine("Carregando FII´s");
-			foreach (var item in p_trs)
+			Console.WriteLine("Market Monitor");
+			while (true)
 			{
-				
-				if (counter > 0 )
+				Console.WriteLine("1 - Listar FII´s");
+				Console.WriteLine("2 - Listar FII´s por Vacância");
+				Console.WriteLine("3 - Calcular Número Mágico Por Nome");
+				Fii fii = new Fii();
+				string option = Console.ReadLine();
+				List<Fii> l_fii = fii.GetFiis();
+				if (option == "1")
 				{
-					Fii p_fii = new Fii();
-					p_htmlDoc.LoadHtml(item.OuterHtml);
-					p_fii.Name = p_htmlDoc.DocumentNode.SelectSingleNode("//td//span//a").InnerText;
-					p_fii.Segment = p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[contains(@ckass,'res_papel')][2]").InnerText;
-					p_fii.Price = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[3]").InnerText);
-					p_fii.FfoYield = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[4]").InnerText);
-					p_fii.DividendYield = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[5]").InnerText);
-					p_fii.P_VP = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[6]").InnerText);
-					p_fii.MarketValue = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[7]").InnerText);
-					p_fii.Liquidity = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[8]").InnerText);
-					p_fii.RealEstateQuantity = int.Parse(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[9]").InnerText);
-					p_fii.PricePerM2 = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[10]").InnerText);
-					p_fii.RentPerM2 = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[11]").InnerText);
-					p_fii.CapRate = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[12]").InnerText);
-					p_fii.AverageVacancy = p_fii.RemoveCharacters(p_htmlDoc.DocumentNode.SelectSingleNode("//tr/td[12]").InnerText);
-					l_fiis.Add(p_fii);
+					Console.WriteLine("{0,10}\t{1,10}\t{2,20}\t{3,10}", "Name", "Average Vacancy", "Segment", "Price");
+					foreach (var item in l_fii)
+					{
+						Console.WriteLine("{0,10}\t{1,10}\t{2,20}\t{3,10}", item.Name,item.AverageVacancy, item.Segment, item.Price);
+					}
 				}
-				counter += 1;
+				if (option == "3")
+				{
+					Console.WriteLine("Nome do FII: ");
+					string p_label = Console.ReadLine();
+					double p_magicNumber = fii.CalculateMagicNumberByName(p_label, l_fii);
+					Console.WriteLine($"NÚmero Mágico {p_label} - {p_magicNumber}");
+				}
+				Console.ReadKey();
 			}
-			Console.WriteLine(l_fiis.Count + " FII´s Carregados");
 			
 		}
 	}
