e239c817001b11ef18ecbd939e4a09d2163225c3
Caior15 <caiorochagoes100@gmail.com>
2021-12-13 20:11:46 -0300

    Created the emergency reserve calculation function

---
+++ Market_Monitor/Finance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Market_Monitor
+{
+	public class Finance
+	{
+		public void CalculateEmergencyReserve(double p_monthlySalary, double p_monthlyCost, double p_monthly_Savings, int p_monthsProtected)
+		{
+			try
+			{
+				Console.Clear();
+				//p_monthly_Savings = p_monthly_Savings / 100;
+				p_monthsProtected = (p_monthsProtected >= 0) ? p_monthsProtected : 12;
+				double p_emergencyReservePurpose = p_monthsProtected * p_monthlyCost;
+				double p_monthlyProfitability = 0.40/ 100;
+				//Console.WriteLine(p_monthlyProfitability);
+				double p_annualProfitability = Math.Round((Math.Pow((1 + p_monthlyProfitability), 12) - 1)*100, 1);
+				double p_valueMonthlySalary = ((p_monthlySalary * p_monthly_Savings) / 100);
+				double p_lastFinalValue = 0;
+				double p_finalValue = 0;
+				double p_monthlyIncome = 0;
+
+
+				Console.WriteLine("{0,10}\t{1,10}\t{2,20}\t{3,15}\t{4,10}\t{5,10}\t{6,10}", "Mês", "Valor Inicial", "Poupança Mensal", "Rentabilidade Mensal", "Valor Final", "Rendimento Mensal($)","Ano");
+				for (int i = 0; p_emergencyReservePurpose > p_lastFinalValue; i++)
+				{
+					int p_mouth = i + 1;
+					double p_year = Math.Round((p_mouth +0.0) / 12, 1);
+					if (p_mouth == 1)
+					{
+						p_lastFinalValue = Math.Round((p_valueMonthlySalary * (1 + p_monthlyProfitability)), 2);
+						Console.WriteLine("{0,10}\t{1,10}\t{2,15}\t{3,20}\t{4,15}\t{5,20}\t{6,19}", p_mouth, "-", p_valueMonthlySalary, p_monthlyProfitability * 100, p_lastFinalValue, "-" , p_year);
+						p_lastFinalValue += p_valueMonthlySalary;
+					}
+					else
+					{
+						p_finalValue = Math.Round(p_lastFinalValue * (1 + p_monthlyProfitability), 2);
+						p_monthlyIncome = Math.Round(p_finalValue - p_lastFinalValue, 2);
+						p_lastFinalValue = Math.Round(p_finalValue + p_valueMonthlySalary, 2);
+
+						Console.WriteLine("{0,10}\t{1,10}\t{2,15}\t{3,20}\t{4,15}\t{5,20}\t{6,19}", p_mouth, p_lastFinalValue, p_valueMonthlySalary, p_monthlyProfitability * 100, p_finalValue, p_monthlyIncome, p_year);
+
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+
+				throw ex;
+			}
+			
+		}
+	}
+}
--- Market_Monitor/Program.cs
+++ Market_Monitor/Program.cs
@@ -14,41 +14,99 @@ namespace Market_Monitor
 			Console.WriteLine("Market Monitor");
 			while (true)
 			{
-				Console.WriteLine("1 - Listar FII´s");
-				Console.WriteLine("2 - Listar FII´s por Vacância");
-				Console.WriteLine("3 - Calcular Número Mágico Por Nome");
-				Console.WriteLine("4 - Selecionar Fii");
-				Fii fii = new Fii();
+				Console.WriteLine("\n***** _MENU_ *****\n1 - Finanças\n2 - FII´s");
 				string option = Console.ReadLine();
-				List<Fii> l_fii = fii.GetFiis();
-				if (option == "1")
+				switch (option)
 				{
-					Console.WriteLine("{0,10}\t{1,10}\t{2,20}\t{3,10}", "Name", "Average Vacancy", "Segment", "Price");
-					foreach (var item in l_fii)
-					{
-						Console.WriteLine("{0,10}\t{1,10}\t{2,20}\t{3,10}", item.Name,item.AverageVacancy, item.Segment, item.Price);
-					}
+					case "1":
+						FinanceMenu();
+						break;
+					case "2":
+						FiiMenu();
+						break;
+					default:
+						break;
 				}
-				if (option =="2")
+			}
+			
+		}
+		public static void FinanceMenu()
+		{
+			Finance finance = new Finance();
+			while (true)
+			{
+				Console.WriteLine("\n***** _FINANCE_MENU_ *****\n1 -  Calculo Reserva de Emergência");
+				string option = Console.ReadLine();
+				switch (option)
 				{
-					Console.WriteLine("Não disponivel");
-				}
-				if (option == "3")
-				{
-					Console.WriteLine("Nome do FII: ");
-					string p_label = Console.ReadLine();
-					double p_magicNumber = fii.CalculateMagicNumberByName(p_label, l_fii);
-					Console.WriteLine($"NÚmero Mágico {p_label} - {p_magicNumber}");
-				}
-				if (option == "4")
-				{
-					Console.WriteLine("Nome do FII: ");
-					string p_label = Console.ReadLine();
+					case "1":
+						Console.WriteLine("Informe os seguintes valores\n\nSalário Mensal(1000.00) | Custo Mensal(1000.00) | Poupaça Mensal(10 %) | Meses Protegidos(12)");
+						Console.WriteLine("Salário Mensal: ");
+						double p_monthlySalary = double.Parse(Console.ReadLine());
+						Console.WriteLine("Custo Mensal: ");
+						double p_monthlyCost = double.Parse(Console.ReadLine());
+						Console.WriteLine("Poupaça Mensal: ");
+						double p_monthly_Savings = double.Parse(Console.ReadLine());
+						Console.WriteLine("Meses Protegidos (Opcional, padrão 12): ");
+						int p_monthsProtected = int.Parse(Console.ReadLine());
 
-					if (!string.IsNullOrEmpty(p_label))
-						fii.GetFiiByName(p_label, l_fii);
+						finance.CalculateEmergencyReserve(p_monthlySalary, p_monthlyCost, p_monthly_Savings, p_monthsProtected);
+						break;
+					default:
+						break;
 				}
-				Console.ReadKey();
+
+			}
+		}
+		public static void FiiMenu()
+		{
+			try
+			{
+				while (true)
+				{
+					Console.WriteLine("1 - Listar FII´s");
+					Console.WriteLine("2 - Listar FII´s por Vacância");
+					Console.WriteLine("3 - Calcular Número Mágico Por Nome");
+					Console.WriteLine("4 - Selecionar Fii");
+					Console.WriteLine("9 - CalculateEmergencyReserve");
+					Fii fii = new Fii();
+					string option = Console.ReadLine();
+					List<Fii> l_fii = fii.GetFiis();
+					if (option == "1")
+					{
+						Console.WriteLine("{0,10}\t{1,10}\t{2,20}\t{3,10}", "Name", "Average Vacancy", "Segment", "Price");
+						foreach (var item in l_fii)
+						{
+							Console.WriteLine("{0,10}\t{1,10}\t{2,20}\t{3,10}", item.Name, item.AverageVacancy, item.Segment, item.Price);
+						}
+					}
+					if (option == "2")
+					{
+						Console.WriteLine("Não disponivel");
+					}
+					if (option == "3")
+					{
+						Console.WriteLine("Nome do FII: ");
+						string p_label = Console.ReadLine();
+						double p_magicNumber = fii.CalculateMagicNumberByName(p_label, l_fii);
+						Console.WriteLine($"NÚmero Mágico {p_label} - {p_magicNumber}");
+					}
+					if (option == "4")
+					{
+						Console.WriteLine("Nome do FII: ");
+						string p_label = Console.ReadLine();
+
+						if (!string.IsNullOrEmpty(p_label))
+							fii.GetFiiByName(p_label, l_fii);
+					}
+
+					Console.ReadKey();
+				}
+			}
+			catch (Exception ex)
+			{
+
+				throw ex;
 			}
 			
 		}
